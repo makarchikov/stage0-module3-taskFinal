ca1b2d49a762f500a3c52c8f975b6a69d84d5cbb
salvadordali99 <152178723+salvadordali99@users.noreply.github.com>
2024-08-12 00:50:56 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            sum +=digit;
+            number /= 10;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double farenheit =  (temperatureCelsius * 9.0 / 5.0) + 32.0;
+        System.out.println(farenheit);
+
     }
 }
